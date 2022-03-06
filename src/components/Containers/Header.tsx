b4fb1563f4d59cb3aaef5a5f9ab68d1721eaf567
frxnpt <62038055+frxnpt@users.noreply.github.com>
2022-03-06 21:14:49 +0100

    Created Header component

---
+++ src/components/Containers/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { InputSearch } from "../Actionables/InputSearch";
+
+export const Header = () => {
+	return (
+		<div className="header">
+			<p>Spotify Finder</p>
+		</div>
+	);
+};
+
+export default Header;
