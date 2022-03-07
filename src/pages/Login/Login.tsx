a67ea184bd27ae0b287afe7e79a80f1be3403c75
frxnpt <62038055+frxnpt@users.noreply.github.com>
2022-03-07 23:38:21 +0100

    Create a login component and saving the access_token in useContext variable

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { signInWithSpotify } from "../../api/signInWithSpotify";
+import { AuthContext } from "../../context/Login/AuthContext";
 
-export const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+const Login = () => {
+	const { authToken, setAuthToken, isAuthenticated, setIsAuthenticated } =
+		useContext(AuthContext);
+	const signIn = async () => {
+		await signInWithSpotify().then((data) => {
+			console.log(data.access_token);
+			setAuthToken(data.access_token);
+      setIsAuthenticated(true);
+		});
+	};
+
+	return (
+		<div>
+			<button onClick={signIn}>Sign in with spotify</button>
+		</div>
+	);
+};
 
 export default Login;
