ea4528343862f1ea36c465ed6ad460d3b504062c
frxnpt <62038055+frxnpt@users.noreply.github.com>
2022-03-07 10:56:07 +0100

    Added small condition to item container

--- src/components/Containers/ItemContainer.tsx
+++ src/components/Containers/ItemContainer.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { printConditionally } from "../../utils/printConditionally";
 
 type ItemContainerProps = {
     title: string;
+	small: boolean;
     children: any;
 }
-export const ItemContainer = ({title, children} : ItemContainerProps) => {
+export const ItemContainer = ({title, children,small} : ItemContainerProps) => {
+	
 	return (
-		<div className="itemContainer">
+		<div className={printConditionally(small, "smallItemContainer","itemContainer")}>
 			<div className="containerTop">
 				<p className="containerTitle">{title}</p>
 			</div>
