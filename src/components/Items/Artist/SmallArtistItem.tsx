f5b4308f5773e128885e6a1204ec5218731b5e4b
frxnpt <62038055+frxnpt@users.noreply.github.com>
2022-03-07 10:55:30 +0100

    Create a component to show Artists in a smaller format

---
+++ src/components/Items/Artist/SmallArtistItem.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Artist } from "../../../api/SpotifyTypes";
+
+type ArtistItemProps = {
+	artist: Artist;
+};
+
+export const SmallArtistItem = ({ artist }: ArtistItemProps) => {
+	return (
+		<div key={artist.id}>
+			<div className="listItem">
+				<img
+					className="itemImg"
+					alt="item pic"
+					src={artist.images[2].url}
+				></img>
+				<div className="itemInfoGroup">
+					<div className="itemInfo">
+						<div>
+							<p className="itemInfoTag">Name</p>
+							<p className="itemInfoContent">{artist.name}</p>
+						</div>
+					</div>
+					<div className="itemInfo">
+						<div>
+							<p className="itemInfoTag">Type</p>
+							<p className="itemInfoContent">{artist.type}</p>
+						</div>
+					</div>
+				</div>
+				<a
+					href={"https://open.spotify.com/" + artist.type + "/" + artist.id}
+					target="_blank"
+					rel="noreferrer"
+					className="itemLink"
+				>
+					Open Spotify
+				</a>
+			</div>
+		</div>
+	);
+};
+export default SmallArtistItem;
