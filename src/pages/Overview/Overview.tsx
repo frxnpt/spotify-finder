816940e98375a8f9a7cc76d7f65915a4d1857706
frxnpt <62038055+frxnpt@users.noreply.github.com>
2022-03-07 23:42:00 +0100

    Start debugging items using the authContext to do the request

--- src/pages/Overview/Overview.tsx
+++ src/pages/Overview/Overview.tsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { getSpotifyItems } from "../../api/getSpotifyItems";
+import { InputSearch } from "../../components/Actionables/InputSearch";
+import BigContainerBox from "../../components/Containers/BigContainerBox";
+import { ItemContainer } from "../../components/Containers/ItemContainer";
+import ItemContainerGroup from "../../components/Containers/ItemContainerGroup";
+import debounce from "lodash.debounce";
+import ListArtists from "../../components/Lists/ListArtists";
+import { AuthContext } from "../../context/Login/AuthContext";
 
 export const Overview = () => {
-  return (
-    <div>Overview</div>
-  )
-}
 	const authContext = useContext(AuthContext);
+	
+	const updateSearch = (searchQuery: string) => {
+		getSpotifyItems(searchQuery, authContext.authToken)
+		.then((data) => {
+			console.log("Fetched data", data)
+		})
+		.catch(err => {
+			alert('Result not found for "' + searchQuery + '" ');
+		});
+		
+	};
+
+	const clearResults = () => {};
+
+	
+	console.log(JSON.stringify(authContext));
+
+	return (
+		<>
+			<InputSearch onSearch={updateSearch} onClear={clearResults}></InputSearch>
+			<BigContainerBox>
+				<ItemContainerGroup>
+					<ItemContainer title="Artists" small={true}>
+						<ListArtists artists={[]} small={true} />
+					</ItemContainer>
+					<ItemContainer title="Albums" small={true}>
+						<ListArtists artists={[]} small={true} />
+					</ItemContainer>
+				</ItemContainerGroup>
+				<ItemContainer title="Tracks" small={false}>
+					<ListArtists artists={[]} small={false} />
+				</ItemContainer>
+			</BigContainerBox>
+		</>
+	);
+};
 
 export default Overview;
