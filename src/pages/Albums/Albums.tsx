ea7d1d3d3aa9b532a84e421f4fa95db488069928
frxnpt <62038055+frxnpt@users.noreply.github.com>
2022-03-08 12:51:42 +0100

    Update Albums page to display the albums

--- src/pages/Albums/Albums.tsx
+++ src/pages/Albums/Albums.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import debounce from "lodash.debounce";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { getSpotifyItems } from "../../api/getSpotifyItems";
+import { InputSearch } from "../../components/Actionables/InputSearch";
+import BigContainerBox from "../../components/Containers/BigContainerBox";
+import { ItemContainer } from "../../components/Containers/ItemContainer";
+import ListAlbums from "../../components/Lists/ListAlbums";
+import ListArtists from "../../components/Lists/ListArtists";
+import { ItemContext } from "../../context/Items/ItemContext";
+import { AuthContext } from "../../context/Login/AuthContext";
 
 export const Albums = () => {
-  return (
-    <div>Album</div>
-  )
-}
+	const authContext = useContext(AuthContext);
+	const itemContext = useContext(ItemContext);
+	const navigate = useNavigate();
 
-export default Albums
+	const clearResults = () => {
+		console.log("borrando");
+		itemContext.setAlbums([]);
+		itemContext.setArtists([]);
+		itemContext.setTracks([]);
+		navigate("/overview");
+	};
+
+	const searchOnSpotifyAPI = (searchQuery: string) => {
+		getSpotifyItems(searchQuery, authContext.authToken)
+			.then((data) => {
+				console.log("Fetched data", data);
+				itemContext.setAlbums(data.albums.items);
+				itemContext.setArtists(data.artists.items);
+				itemContext.setTracks(data.tracks.items);
+				//itemContext.setSpotifyItems(data as SpotifyItemsResponse)
+			})
+			.catch((err) => {
+				alert('Result not found for "' + searchQuery + '" ');
+			});
+	};
+
+	const updateSearch = debounce(searchOnSpotifyAPI, 0);
+	return (
+		<>
+			<InputSearch onSearch={updateSearch} onClear={clearResults}></InputSearch>
+			<BigContainerBox>
+				<ItemContainer
+					title="Albums"
+					linkText="Go Back"
+					linkRef="/overview"
+					small={false}
+				>
+					<ListAlbums albums={itemContext.albums} small={false} />
+				</ItemContainer>
+			</BigContainerBox>
+		</>
+	);
+};
+
+export default Albums;
