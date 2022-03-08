80ddb9cf3be3b3f1918244fcf915e21a92c447b2
frxnpt <62038055+frxnpt@users.noreply.github.com>
2022-03-08 07:44:14 +0100

    Change item container format to print small ones properly

--- src/components/Containers/ItemContainer.tsx
+++ src/components/Containers/ItemContainer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { printConditionally } from "../../utils/printConditionally";
 
 type ItemContainerProps = {
@@ -6,12 +7,13 @@ type ItemContainerProps = {
 	small: boolean;
     children: any;
 }
-export const ItemContainer = ({title, children,small} : ItemContainerProps) => {
-	
+export const ItemContainer = ({title, small, children} : ItemContainerProps) => {
+	const link = "/"+title
 	return (
-		<div className={printConditionally(small, "smallItemContainer","itemContainer")}>
+		<div className={printConditionally(small,"smallItemContainer","itemContainer")}>
 			<div className="containerTop">
 				<p className="containerTitle">{title}</p>
+				<Link to={link.toLowerCase()} >ViewAll</Link>
 			</div>
             {children}
 		</div>
