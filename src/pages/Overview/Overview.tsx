9824804e585c225a9a8414f0eca501a146c6bf7b
frxnpt <62038055+frxnpt@users.noreply.github.com>
2022-03-08 12:52:14 +0100

    Modified overview to show items properly

--- src/pages/Overview/Overview.tsx
+++ src/pages/Overview/Overview.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { getSpotifyItems } from "../../api/getSpotifyItems";
 import { InputSearch } from "../../components/Actionables/InputSearch";
 import BigContainerBox from "../../components/Containers/BigContainerBox";
@@ -8,52 +8,74 @@ import debounce from "lodash.debounce";
 import ListArtists from "../../components/Lists/ListArtists";
 import { AuthContext } from "../../context/Login/AuthContext";
 import { ItemContext } from "../../context/Items/ItemContext";
+import { printConditionally } from "../../utils/printConditionally";
+import ListAlbums from "../../components/Lists/ListAlbums";
+import ListTracks from "../../components/Lists/ListTracks";
 
 export const Overview = () => {
 	const authContext = useContext(AuthContext);
 	const itemContext = useContext(ItemContext);
 
 	const clearResults = () => {
-		console.log("borrando")
+		console.log("borrando");
 		itemContext.setAlbums([]);
 		itemContext.setArtists([]);
 		itemContext.setTracks([]);
 	};
-	
+
 	const searchOnSpotifyAPI = (searchQuery: string) => {
+		console.log(searchQuery);
 		getSpotifyItems(searchQuery, authContext.authToken)
-		.then((data) => {
-			console.log("Fetched data", data)
-			itemContext.setAlbums(data.albums.items);
-			itemContext.setArtists(data.artists.items);
-			itemContext.setTracks(data.tracks.items);
-			//itemContext.setSpotifyItems(data as SpotifyItemsResponse)
-		})
-		.catch(err => {
-			alert('Result not found for "' + searchQuery + '" ');
-		});
+			.then((data) => {
+				console.log("Fetched data", data);
+				itemContext.setAlbums(data.albums.items);
+				itemContext.setArtists(data.artists.items);
+				itemContext.setTracks(data.tracks.items);
+			})
+			.catch((err) => {
+				alert('Result not found for "' + searchQuery + '" ');
+			});
 	};
 
-	
-	const updateSearch = debounce(searchOnSpotifyAPI, 500);	
-	
-	console.log(JSON.stringify(authContext));
-	console.log(JSON.stringify(itemContext.artists))
+	const updateSearch = debounce(searchOnSpotifyAPI, 500);
 
 	return (
 		<>
 			<InputSearch onSearch={updateSearch} onClear={clearResults}></InputSearch>
 			<BigContainerBox>
 				<ItemContainerGroup>
-					<ItemContainer title="Artists" small={true}>
-						<ListArtists artists={itemContext.artists} limit={1} small={false} />
+					<ItemContainer
+						title="Artists"
+						linkText="View all"
+						linkRef="/artists"
+						small={true}
+					>
+						<ListArtists
+							artists={itemContext.artists}
+							limit={2}
+							small={true}
+						/>
 					</ItemContainer>
-					<ItemContainer title="Albums" small={true}>
-						<ListArtists artists={itemContext.artists} limit={1} small={false} />
+					<ItemContainer
+						title="Albums"
+						linkText="View all"
+						linkRef="/albums"
+						small={true}
+					>
+						<ListAlbums
+							albums={itemContext.albums}
+							limit={2}
+							small={true}
+						/>
 					</ItemContainer>
 				</ItemContainerGroup>
-				<ItemContainer title="Tracks" small={false}>
-					<ListArtists artists={itemContext.artists} small={false} />
+				<ItemContainer
+					title="Tracks"
+					linkText="View all"
+					linkRef="/artists"
+					small={false}
+				>
+					<ListTracks tracks={itemContext.tracks} small={false} />
 				</ItemContainer>
 			</BigContainerBox>
 		</>
