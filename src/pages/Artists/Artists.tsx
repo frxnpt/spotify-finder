026c1d85bfd6aba948350dc309612ba5ca6b918a
frxnpt <62038055+frxnpt@users.noreply.github.com>
2022-03-08 12:52:00 +0100

    Update artist page to only show artists

--- src/pages/Artists/Artists.tsx
+++ src/pages/Artists/Artists.tsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import debounce from 'lodash.debounce';
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { getSpotifyItems } from '../../api/getSpotifyItems';
+import { InputSearch } from '../../components/Actionables/InputSearch';
+import BigContainerBox from '../../components/Containers/BigContainerBox';
+import { ItemContainer } from '../../components/Containers/ItemContainer';
+import ItemContainerGroup from '../../components/Containers/ItemContainerGroup';
+import ListArtists from '../../components/Lists/ListArtists';
+import { ItemContext } from '../../context/Items/ItemContext';
+import { AuthContext } from '../../context/Login/AuthContext';
 
 export const Artists = () => {
+  const authContext = useContext(AuthContext);
+  const itemContext = useContext(ItemContext);
+  const navigate = useNavigate()
+
+	const clearResults = () => {
+		console.log("borrando")
+		itemContext.setAlbums([]);
+		itemContext.setArtists([]);
+		itemContext.setTracks([]);
+		navigate("/overview")
+    
+	};
+	
+	const searchOnSpotifyAPI = (searchQuery: string) => {
+		getSpotifyItems(searchQuery, authContext.authToken)
+		.then((data) => {
+			console.log("Fetched data", data)
+			itemContext.setAlbums(data.albums.items);
+			itemContext.setArtists(data.artists.items);
+			itemContext.setTracks(data.tracks.items);
+			//itemContext.setSpotifyItems(data as SpotifyItemsResponse)
+		})
+		.catch(err => {
+			alert('Result not found for "' + searchQuery + '" ');
+		});
+	};
+
+	
+	const updateSearch = debounce(searchOnSpotifyAPI, 0);	
   return (
-    <div>Artists</div>
+    <>
+			<InputSearch onSearch={updateSearch} onClear={clearResults}></InputSearch>
+			<BigContainerBox>
+					<ItemContainer title="Artists" linkText="Go Back" linkRef="/overview"small={false}>
+						<ListArtists artists={itemContext.artists} small={false} />
+					</ItemContainer>
+			</BigContainerBox>
+		</>
   )
 }
 export default Artists;
