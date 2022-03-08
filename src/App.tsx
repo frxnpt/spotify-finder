ef4bd32dd638925afd51b6e72d9f97047e593246
frxnpt <62038055+frxnpt@users.noreply.github.com>
2022-03-08 07:43:22 +0100

    add itemcontext to all the routes

--- src/App.tsx
+++ src/App.tsx
@@ -9,72 +9,83 @@ import ProtectedRoute from "./components/Auth/ProtectedRoute";
 import "./styles/_global.scss";
 import Header from "./components/Containers/Header";
 import { AuthContext } from "./context/Login/AuthContext";
+import { Album, Artist, Track } from "./api/SpotifyTypes";
+import { ItemContext } from "./context/Items/ItemContext";
 //TODO: move routes to react.js file and import them here using a map
 function App() {
 	const [authToken, setAuthToken] = useState("");
 	const [isAuthenticated, setIsAuthenticated] = useState(false)
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	//const [spotifyItems, setSpotifyItems] = useState({} as SpotifyItemsResponse);
+	const [albums, setAlbums] = useState<Album[]>([]);
+	const [artists, setArtists] = useState<Artist[]>([]);
+	const [tracks, setTracks] = useState<Track[]>([]);	
 
 	return (
-		<AuthContext.Provider value={{authToken, setAuthToken, isAuthenticated, setIsAuthenticated}}>
-			<BrowserRouter>
-				<div className="container">
-					<Header />
-					<Routes>
-						<Route
-							path="/overview"
-							element={
-								<ProtectedRoute isAuthenticated={isAuthenticated}>
-									<Overview />
-								</ProtectedRoute>
-							}
-						/>
-						<Route
-							path="/artists"
-							element={
-								<ProtectedRoute isAuthenticated={isAuthenticated}>
-									<Artists />
-								</ProtectedRoute>
-							}
-						></Route>
-						<Route
-							path="/albums"
-							element={
-								<ProtectedRoute isAuthenticated={isAuthenticated}>
-									<Albums />
-								</ProtectedRoute>
-							}
-						></Route>
-						<Route
-							path="/tracks"
-							element={
-								<ProtectedRoute isAuthenticated={isAuthenticated}>
-									<Tracks />
-								</ProtectedRoute>
-							}
-						></Route>
-						<Route
-							path="/login"
-							element={
-								<ProtectedRoute
-									redirectPath="/overview"
-									isAuthenticated={!isAuthenticated}
-								>
-									<Login />
-								</ProtectedRoute>
-							}
-						></Route>
-						<Route
-							path="*"
-							element={
-								<ProtectedRoute isAuthenticated={isAuthenticated}>
-									<Navigate to="/overview" replace />
-								</ProtectedRoute>
-							}
-						/>
-					</Routes>
-				</div>
-			</BrowserRouter>
-		</AuthContext.Provider>
+		<ItemContext.Provider value={{ albums, setAlbums, tracks, setTracks, artists, setArtists}}>
+			<AuthContext.Provider
+				value={{ authToken, setAuthToken, isAuthenticated, setIsAuthenticated }}
+			>
+				<BrowserRouter>
+					<div className="container">
+						<Header />
+						<Routes>
+							<Route
+								path="/overview"
+								element={
+									<ProtectedRoute isAuthenticated={isAuthenticated}>
+										<Overview />
+									</ProtectedRoute>
+								}
+							/>
+							<Route
+								path="/artists"
+								element={
+									<ProtectedRoute isAuthenticated={isAuthenticated}>
+										<Artists />
+									</ProtectedRoute>
+								}
+							></Route>
+							<Route
+								path="/albums"
+								element={
+									<ProtectedRoute isAuthenticated={isAuthenticated}>
+										<Albums />
+									</ProtectedRoute>
+								}
+							></Route>
+							<Route
+								path="/tracks"
+								element={
+									<ProtectedRoute isAuthenticated={isAuthenticated}>
+										<Tracks />
+									</ProtectedRoute>
+								}
+							></Route>
+							<Route
+								path="/login"
+								element={
+									<ProtectedRoute
+										redirectPath="/overview"
+										isAuthenticated={!isAuthenticated}
+									>
+										<Login />
+									</ProtectedRoute>
+								}
+							></Route>
+							<Route
+								path="*"
+								element={
+									<ProtectedRoute isAuthenticated={isAuthenticated}>
+										<Navigate to="/overview" replace />
+									</ProtectedRoute>
+								}
+							/>
+						</Routes>
+					</div>
+				</BrowserRouter>
+			</AuthContext.Provider>
+		</ItemContext.Provider>
 	);
 }
 
