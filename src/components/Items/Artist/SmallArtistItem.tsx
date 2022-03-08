a763b620b9e4c30ac1d02dc20a3f4ce825d05313
frxnpt <62038055+frxnpt@users.noreply.github.com>
2022-03-08 12:51:01 +0100

    Creating small artist Item

--- src/components/Items/Artist/SmallArtistItem.tsx
+++ src/components/Items/Artist/SmallArtistItem.tsx
@@ -1,29 +1,25 @@
 import React from "react";
 import { Artist } from "../../../api/SpotifyTypes";
 import { getArtistOrAlbumImageURL } from "../../../utils/getItemImage";
-
+import '../Items.scss'
 type ArtistItemProps = {
 	artist: Artist;
 };
 
 export const SmallArtistItem = ({ artist }: ArtistItemProps) => {
-	const artistImage = getArtistOrAlbumImageURL(artist)
+	const artistImage = getArtistOrAlbumImageURL(artist);
 	return (
 		<div key={artist.id}>
 			<div className="listItem">
-				<img
-					className="itemImg"
-					alt="item pic"
-					src={artistImage}
-				></img>
+				<img className="itemImg" alt="item pic" src={artistImage}></img>
 				<div className="itemInfoGroup">
-					<div className="itemInfo">
+					<div className="smallItemInfo">
 						<div>
 							<p className="itemInfoTag">Name</p>
 							<p className="itemInfoContent">{artist.name}</p>
 						</div>
 					</div>
-					<div className="itemInfo">
+					<div className="smallItemInfo">
 						<div>
 							<p className="itemInfoTag">Type</p>
 							<p className="itemInfoContent">{artist.type}</p>
