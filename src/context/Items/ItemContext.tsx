1986b5efdb2fd62ffb38284532d3b655180030bd
frxnpt <62038055+frxnpt@users.noreply.github.com>
2022-03-08 07:39:37 +0100

    Change itemContext format and props

--- src/context/Items/ItemContext.tsx
+++ src/context/Items/ItemContext.tsx
@@ -1,6 +1,15 @@
 import React, { createContext } from 'react'
-import { SpotifyItemsResponse } from '../../api/SpotifyTypes';
+import { Album, Artist, Track } from '../../api/SpotifyTypes';
 
+export type ItemContextProps = {
+	//spotifyItems: SpotifyItemsResponse
+	//setSpotifyItems: Dispatch<SetStateAction<SpotifyItemsResponse>>
+    albums: Album[]
+    setAlbums: React.Dispatch<React.SetStateAction<Album[]>>
+    tracks: Track[]
+    setTracks: React.Dispatch<React.SetStateAction<Track[]>>
+    artists: Artist[]; 
+    setArtists: React.Dispatch<React.SetStateAction<Artist[]>>
+}
 
-
-export const ItemContext = createContext({} as SpotifyItemsResponse);
+export const ItemContext = createContext({} as ItemContextProps);
