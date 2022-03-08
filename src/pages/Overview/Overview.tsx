664dcc3674b4cee70c5f886592d664211ede71ac
frxnpt <62038055+frxnpt@users.noreply.github.com>
2022-03-08 07:40:40 +0100

    Save spotify items in usecontext state and clear it when deleting input's content

--- src/pages/Overview/Overview.tsx
+++ src/pages/Overview/Overview.tsx
@@ -7,25 +7,38 @@ import ItemContainerGroup from "../../components/Containers/ItemContainerGroup";
 import debounce from "lodash.debounce";
 import ListArtists from "../../components/Lists/ListArtists";
 import { AuthContext } from "../../context/Login/AuthContext";
+import { ItemContext } from "../../context/Items/ItemContext";
 
 export const Overview = () => {
 	const authContext = useContext(AuthContext);
+	const itemContext = useContext(ItemContext);
+
+	const clearResults = () => {
+		console.log("borrando")
+		itemContext.setAlbums([]);
+		itemContext.setArtists([]);
+		itemContext.setTracks([]);
+	};
 	
-	const updateSearch = (searchQuery: string) => {
+	const searchOnSpotifyAPI = (searchQuery: string) => {
 		getSpotifyItems(searchQuery, authContext.authToken)
 		.then((data) => {
 			console.log("Fetched data", data)
+			itemContext.setAlbums(data.albums.items);
+			itemContext.setArtists(data.artists.items);
+			itemContext.setTracks(data.tracks.items);
+			//itemContext.setSpotifyItems(data as SpotifyItemsResponse)
 		})
 		.catch(err => {
 			alert('Result not found for "' + searchQuery + '" ');
 		});
-		
 	};
 
-	const clearResults = () => {};
-
+	
+	const updateSearch = debounce(searchOnSpotifyAPI, 500);	
 	
 	console.log(JSON.stringify(authContext));
+	console.log(JSON.stringify(itemContext.artists))
 
 	return (
 		<>
@@ -33,14 +46,14 @@ export const Overview = () => {
 			<BigContainerBox>
 				<ItemContainerGroup>
 					<ItemContainer title="Artists" small={true}>
-						<ListArtists artists={[]} small={true} />
+						<ListArtists artists={itemContext.artists} limit={1} small={false} />
 					</ItemContainer>
 					<ItemContainer title="Albums" small={true}>
-						<ListArtists artists={[]} small={true} />
+						<ListArtists artists={itemContext.artists} limit={1} small={false} />
 					</ItemContainer>
 				</ItemContainerGroup>
 				<ItemContainer title="Tracks" small={false}>
-					<ListArtists artists={[]} small={false} />
+					<ListArtists artists={itemContext.artists} small={false} />
 				</ItemContainer>
 			</BigContainerBox>
 		</>
